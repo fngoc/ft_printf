66bd1469d407239ea786c2c68b6457bb6556dbf1
Vitaliy Khaydarov <vitmarkha@icloud.com>
2020-11-25 22:26:17 +0300

    Update ft_printf.c

--- libftprintf/ft_printf.c
+++ libftprintf/ft_printf.c
@@ -28,17 +28,10 @@ int	ft_printf(const char *format, ...)
 		++format;
 	}
 	va_end(p.ap);
+//	printf("Parser:\n%c\n%d\n%d\n%c\n", p.flags, p.width, p.number_after_dot, p.type);
 	return (p.count);
 }
 
-//			if (*format == 'd' || *format == 'i')
-//				ft_putnbr_fd(va_arg(ap, int), 1);
-//			if (*format == 's')
-//				ft_putstr_fd(va_arg(ap, char *), 1);
-//			if (*format == 'c')
-//				ft_putchar_fd(va_arg(ap, int), 1);
-//			if (*format == '%')
-//				ft_putchar_fd('%', 1);
 //			if (*format == 'p')
 //				ft_print_adress(va_arg(ap, void *));
 //			if (*format == 'u')
@@ -47,11 +40,3 @@ int	ft_printf(const char *format, ...)
 //				ft_print_hex_little(va_arg(ap, int));
 //			if (*format == 'X')
 //				ft_print_hex_big(va_arg(ap, int));
-//			if (*format++ == '.' && (str = (char *) format))
-//			{
-//				if (ft_isdigit(*str) && (tmp = ft_atoi(format)))
-//					while (ft_isdigit(*format))
-//						format += 1;
-//				if (*format == 's')
-//					ft_putstr_fd_print_int_str(va_arg(ap, char *), 1, tmp);
-//			}
